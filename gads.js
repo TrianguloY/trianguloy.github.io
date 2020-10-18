48fbd32f037944271973219a9b7b9b28bda0b4f8
TrianguloY <correo--correo@hotmail.com>
2020-10-18 23:53:34 +0200

    Update gads.js
    Back to the original message (now that it is correctly blocked again)

--- gads.js
+++ gads.js
@@ -1,15 +1,15 @@
 /*
  * This is a pro adblock message, to show users that ads are displaying and they should use an adblock.
- * If they already are, the name of the file (ads.js) and/or the class of the message (bottomAd) will be blocked by the adblock and so those users won't see anything!
+ * If they already are, the name of the file (gads.js) and/or the class of the message (bottomAd) will be blocked by the adblock and so those users won't see anything!
  * 
  * You can use it in your own page if you want, just append:
- *     <script src="https://trianguloy.github.io/ads.js" type="text/javascript"></script>
+ *     <script src="https://trianguloy.github.io/gads.js" type="text/javascript"></script>
  * anywhere, and the message will be shown in that location if the user is not using any adblock.
  */
 
 var domString = `<div class="bottomAd" style="padding: 10px; background: #500000; text-align: center; font-weight: bold; color: #fff; border-radius: 5px;">
-(!) You may not be using an adblocker, or your current one isn't configured to hide cosmetic ads. (This is the case of mobile devices, not desktop browsers.)
-<br>If you aren't using one consider it, they are available for almost all commercial browsers and will remove almost all ads and trackers from the web!
+(!) You are not using an adblocker, you are surfing the web full of ads (and companies may be using your data to target and/or track you). 
+<br>Consider using an adblock, they are available for almost all commercial browsers and will remove almost all ads from the web!
 </div>`;
 
 var div = document.createElement('div');
